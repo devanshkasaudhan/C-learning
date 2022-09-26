1be753038d17d6e7888ca567c023d79def6efdce
Devansh <devanshkasaudhan12334@gmail.com>
2022-09-26 19:32:47 +0530

    "adding more files"

---
+++ GFG_CODES/arraySortedOrNot.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+bool arrayIsSortedOrNot(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]< arr[i-1])
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+int main(){
+    int n; 
+    cout << "Enter the array length \n";
+    cin >> n;
+    int arry[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter " << i << "th element of array \n";
+        cin >> arry[i];
+    }
+
+    if ( arrayIsSortedOrNot(arry,n)==false)
+    {
+        cout << "not sorted";
+    }
+    else{
+        cout << "sorted"; 
+
+    }
+
+    return 0;
+}
--- Learning CPP (code with harry)/27.cpp
+++ Learning CPP (code with harry)/27.cpp
@@ -1,10 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class employee
 {
 private:
-    
 public:
     int id, mobile_no;
     float salary;
@@ -14,21 +13,21 @@ public:
 
 employee::employee(int a, long int b, float c)
 {
-    id=a;
-    mobile_no=b;
-    salary=c;
+    id = a;
+    mobile_no = b;
+    salary = c;
 }
 
 employee::~employee()
 {
 }
 
-
-int main(){
-    employee harry(1,8074115709,1500) ,devansh(2,9458057843,20000), arpit(3,9696191101,20000);
-    cout<<harry.salary<<endl;
-    cout<<devansh.mobile_no<<endl;
-    cout<<arpit.id<<endl;
+int main()
+{
+    employee harry(1, 8074115709, 1500), devansh(2, 9458057843, 20000), arpit(3, 9696191101, 20000);
+    cout << harry.salary << endl;
+    cout << devansh.mobile_no << endl;
+    cout << arpit.id << endl;
 
     return 0;
 }
